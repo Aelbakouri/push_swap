cd97534a614a515f95efc03ef4b63b872aa91c0b
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-12-27 17:17:02 +0100

    just a commit

--- src/sort_tree.c
+++ src/sort_tree.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_tiny.c                                        :+:      :+:    :+:   */
+/*   sort_tree.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-bako <ael-bako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/21 15:51:59 by ael-bako          #+#    #+#             */
-/*   Updated: 2022/12/27 17:02:41 by ael-bako         ###   ########.fr       */
+/*   Created: 2022/12/27 17:13:04 by ael-bako          #+#    #+#             */
+/*   Updated: 2022/12/27 17:15:17 by ael-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/push_swap.h"
 
-#include "push_swap.h"
-
-/* find_highest_index:
-*	Returns the highest index in a stack.
-*/
-static int	find_highest_index(t_stack *stack)
+static int	highest_index(t_list *stack)
 {
 	int		index;
 
@@ -30,30 +26,17 @@ static int	find_highest_index(t_stack *stack)
 	return (index);
 }
 
-/* tiny_sort:
-*	Sorts a stack of 3 numbers in 2 or fewer moves. The sorting is done by index
-*	rather than value. Example:
-*		values:		 0	 9	 2
-*		indexes:	[1]	[3]	[2]
-*	Solution, 2 moves:
-*	rra:
-*		values:		 2	 0	 9
-*		indexes:	[2]	[1]	[3]
-*	sa:
-*		values:		 0	 2	 9
-*		indexes:	[1]	[2]	[3]
-*/
-void	tiny_sort(t_stack **stack)
+void	tiny_sort(t_list **stack)
 {
 	int		highest;
 
 	if (is_sorted(*stack))
 		return ;
-	highest = find_highest_index(*stack);
+	highest = highest_index(*stack);
 	if ((*stack)->index == highest)
-		do_ra(stack);
+		ra(stack);
 	else if ((*stack)->next->index == highest)
-		do_rra(stack);
+		rra(stack);
 	if ((*stack)->index > (*stack)->next->index)
-		do_sa(stack);
+		sa(stack);
 }
