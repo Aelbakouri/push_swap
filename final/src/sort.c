7eb7e24748f15558a19c7696f21ee2a959802b75
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-12-29 23:34:45 +0100

    just  a commit

--- final/src/sort.c
+++ final/src/sort.c
@@ -6,11 +6,42 @@
 /*   By: ael-bako <ael-bako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 14:10:55 by ael-bako          #+#    #+#             */
-/*   Updated: 2022/12/29 22:24:54 by ael-bako         ###   ########.fr       */
+/*   Updated: 2022/12/29 22:32:38 by ael-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+/* sort tree*/
+static int	highest_index(t_list *stack)
+{
+	int		index;
+
+	index = stack->index;
+	while (stack)
+	{
+		if (stack->index > index)
+			index = stack->index;
+		stack = stack->next;
+	}
+	return (index);
+}
+
+void	sort_tree(t_list **stack)
+{
+	int		highest;
+
+	if (stack_is_sorted(*stack))
+		return ;
+	highest = highest_index(*stack);
+	if ((*stack)->index == highest)
+		ra(stack);
+	else if ((*stack)->next->index == highest)
+		rra(stack);
+	if ((*stack)->index > (*stack)->next->index)
+		sa(stack);
+}
+
+/*end*/
 
 void	push_and_save_three(t_list **stack_a, t_list **stack_b)
 {
--- final/src/utils2.c
+++ final/src/utils2.c
@@ -1,16 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cost.c                                             :+:      :+:    :+:   */
+/*   utils2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-bako <ael-bako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/21 15:51:12 by ael-bako          #+#    #+#             */
-/*   Updated: 2022/12/29 22:24:25 by ael-bako         ###   ########.fr       */
+/*   Created: 2022/12/21 15:51:28 by ael-bako          #+#    #+#             */
+/*   Updated: 2022/12/29 22:38:32 by ael-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+int	is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	is_sign(char c)
+{
+	return (c == '+' || c == '-');
+}
+
+int	nb_cmp(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i;
+	if (s1[i] == '+')
+	{
+		if (s2[j] != '+')
+			i++;
+	}
+	else
+	{
+		if (s2[j] == '+')
+			j++;
+	}
+	while (s1[i] && s2[j] && s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
+}
 
 void	get_cost(t_list **stack_a, t_list **stack_b)
 {
